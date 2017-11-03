e0467c62a35bf2bbcb109541c8894ddfe0bf2cc4
Storesau <Storesau@gmail.com>
2017-11-03 21:19:28 +0100

    Translate from py2.7 to py3

--- urban.py
+++ urban.py
@@ -1,22 +1,27 @@
 from bs4 import BeautifulSoup
 import lxml.html
-import urllib.request
+import requests
 
-def urban(term):
+try:
+	def urban(term):
 	url = "http://urbandictionary.com/define.php?term={0}".format(term)
-	resource = BeautifulSoup(urllib.request.urlopen(url), "lxml")
+	resource = BeautifulSoup(requests.get(url), "lxml")
 	content = resource.find('div', {"class":"meaning"}).text.strip()
-	strlen = len(content.split())
+	strlen = len(content)
 	string = content
 	splits = 1
 
 	if strlen > 75:
 		string = content.split(".")[0]
 		for x in range(splits):
-			if strlen>20:
+			if strlen > 20:
 				string += content.split(".")[splits]
 				splits += 1
 
 		string += "."
 	print(string)
 	return string
+
+except Exception as e:
+	print(e) 
+	return "Shit the bed, contact Maker!"
