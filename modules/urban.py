61ed8d2af3f07a5b4955f58ce547820faa32b406
svimanet <esj002@uib.no>
2018-02-06 10:16:15 +0100

    fixed urban bug, made simpler

--- modules/urban.py
+++ modules/urban.py
@@ -7,24 +7,13 @@ def urban(term):
 		url = "http://urbandictionary.com/define.php?term={0}".format(term)
 		resource = BeautifulSoup(requests.get(url).content, "lxml")
 		content = resource.find('div', {"class":"meaning"}).text.strip()
-		strlen = len(content)
-		string = content
-		splits = 1
-
-		if strlen > 75:
-			string = content.split(".")[0]
-			for x in range(splits):
-				if strlen > 20:
-					string += content.split(".")[splits]
-					splits += 1
-
-			string += "."
-
-		print(string)
-		return string
+		print(content)
+		return content
 	
 	except Exception as e:
+		print(e)
 		return "Shit the bed, contact Maker!"
+
 
 
 
