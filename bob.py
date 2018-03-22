ce9fa25bcbd1e65e36ce5c27e9edceacf8879a60
svimanet <esj002@uib.no>
2018-03-22 15:43:44 +0100

    added .gitignore for config

--- bob.py
+++ bob.py
@@ -1,7 +1,15 @@
 from modules import irc_bob
 import os
 
+# Check if the config file exists or no
 directory = os.path.dirname(os.path.realpath(__file__))
+file = "{}/bob.config".format(directory)
+if not os.path.isfile("bob.config"):
+	data = input()
+	with open(file, "w+") as data_file:
+		data_file.write(data, encoding="UTF-8")
+	print("wrote " + directory + "/bob.config")
+
 info = open("{}/config".format(directory)).read().split(", ")
 nick = info[0]
 server = info[1]
