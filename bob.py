9781f0f44c23bbb3ec9e4d4fe7469e4a8659edec
svimanet <esj002@uib.no>
2018-03-22 15:57:11 +0100

    change gitignore and default config

--- bob.py
+++ bob.py
@@ -1,19 +1,19 @@
 from modules import irc_bob
 import os
 
-# Check if the config file exists or no
+# Check if the config file exists or no.
+# If it doensn't exist, create on based on u.inp.
 directory = os.path.dirname(os.path.realpath(__file__))
-file = "{}/bob.config".format(directory)
-if not os.path.isfile("bob.config"):
-	data = input()
+file = "{}/config.conf".format(directory)
+if not os.path.isfile("config.conf"):
+	print("Creating default config:")
+	server = input("server  > ")
+	channel = input("channel > ")
+	data = "Bob {} {} 6697".format(server, channel)
+
 	with open(file, "w+") as data_file:
-		data_file.write(data, encoding="UTF-8")
-	print("wrote " + directory + "/bob.config")
+		data_file.write(data)
+		print("wrote " + file)
 
-info = open("{}/config".format(directory)).read().split(", ")
-nick = info[0]
-server = info[1]
-channel = info[2]
-iBob = irc_bob.I_Bob(nick, server, channel, 6697)
-
-iBob.run()
+#iBob = irc_bob.I_Bob(nick, server, channel, 6697)
+#iBob.run()
