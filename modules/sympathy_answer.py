9019a83ce0c129ef26e099d4e31a41cbde074ff2
Orest Karpiak <orekpk@gmail.com>
2019-10-05 22:40:41 +0300

    create sympathy_answer.py module
    I tried to create a cascade for sympathetic answers chat. Hope you like it.

---
+++ modules/sympathy_answer.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def aww(feel_for_ya):
+    print("How was your day today? Good? (\'y\'/'\'n\')"
+    if 'y' in message.lower():
+        print("I'm so happy for you! What was so special about today?")
+        if message.lower():
+            print("have a nice day:)")
+    if 'n' in message.lower():
+        print("Express your feelings to me. I'm all ears.")
+        if message.lower():
+            print("I feel for you. Be well:)")
+            
